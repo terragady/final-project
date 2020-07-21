ff65f7b9065a573f831a3754c63533648d2a2c2f
Tionbai <64463370+Tionbai@users.noreply.github.com>
2020-07-21 17:36:44 +0200

    done_for_today

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,9 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Board from './components/Board';
 import Dice from './components/Dice';
-import io from 'socket.io-client';
+import socket from 'socket.io-client';
 import { v4 as uuid } from 'uuid';
-
-const socket = io.connect('http://localhost:8080');
+const io = socket('http://localhost:8080');
 
 // socket.on('message', message => {
 //   console.log(message);
@@ -17,20 +16,35 @@ function App() {
   const [inGame, setInGame] = useState(false);
   const [roomId, setRoomId] = useState(false);
   const [roomInput, setRoomInput] = useState(false);
+  const [name, setName] = useState(false);
 
-  socket.on('room created', id => {
+
+  useEffect(() => {
+    io.once('Joined room', () => console.log('A player has joined the room'));
+  },[name])
+  const createRoom = async () => {
+    // const id = uuid()
+    const id = 1
     setRoomId(id);
-    console.log(roomId);
-  });
-
-  const createRoom = () => {
-    socket.emit('create new room', uuid());
+    const name = prompt('Enter your name:')
+    io.emit('New room', id, name);
+    console.log(id);
+    io.once('Welcome message', (x) => console.log(x)) 
   }
 
   const joinRoom = (e) => {
     e.preventDefault();
-    socket.emit('join existing room', roomInput);
+    const id = 1;
+    setRoomId(1);
+    document.querySelector('.input--join').value = '';
+    io.emit('Join room', roomInput);
+    // const name = prompt('Enter your name:')
+    // io.once('Joined room', () => console.log('A player has joined the room'));
+    setRoomInput('')
+    setName('Tina')
   }
+
+
 
   return (
     <main className="App">
@@ -39,7 +53,7 @@ function App() {
         : (<section className="App--buttons">
              <button className="button--start" onClick={createRoom}>New room</button>
              <form onSubmit={joinRoom}>
-               <input onChange={(e) => setRoomInput(e.target.value)}/>
+               <input className="input--join" type="text" onChange={(e) => setRoomInput(e.target.value)} />
                <button className="button--join" type="submit" >Join existing room</button>
              </form>
            </section>)
--- server.js
+++ server.js
@@ -1,22 +1,31 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-const path = require('path')
+const path = require('path');
 
-const socketio = require('socket.io');
-const io = socketio(server);
-
+const socket = require('socket.io');
+const io = socket(server);
 // app.get('/setCookie', (req, res)=> //set cookie and redirect
 //   res.redirect('')
 // )
 app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + 'client/build/index.html');
+});
 
 io.on('connection', socket => {
-  socket.on('create new room', roomId => {
-      socket.join(roomId);
-      socket.to(roomId).emit('room created', roomId)
-    });
-  socket.on('join existing room', roomId => {socket.join(roomId); socket.to(roomId).emit('A player has joined')});
+  socket.on('New room', (roomId) => {
+    socket.join(roomId);
+    io.in(roomId).emit('Welcome message', 'helloooo');
+  });
+
+  socket.on('Join room', roomId => {
+    socket.join(roomId);
+    io.in(roomId).emit('Joined room', roomId)
+
+  });
+  
+
 });
 
 const port = 8080;
