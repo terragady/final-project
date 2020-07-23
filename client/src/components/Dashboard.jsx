6fcc647387973d1ad7f7fe5436ee7781f54eef9a
Tionbai <64463370+Tionbai@users.noreply.github.com>
2020-07-23 23:51:38 +0200

    css on dashboard

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -10,21 +10,21 @@ export default function Dashboard() {
     <>
       <h3>Dashboard:</h3>
       <section className="center--dashboard">
-        <div className="center--dashboard__players">
-          <h3>General information:</h3>
+        <div className="center--dashboard__block">
+          <h3 className="center--dashboard__title">Players:</h3>
           {state.loaded
             ? Object.keys(state.players).map(player => (
               <div key={uuid()} className="player-wrapper">
-                <h3 style={{ color: state.players[player].color }}>
+                <h3 className="center--dashboard__player-info"style={{ color: state.players[player].color }}>
                   {state.players[player].name}
                 </h3>
-                <p>{`Account balance: $${state.players[player].accountBalance}M`}</p>
+                <p className="center--dashboard__player-info">{`Account balance: $${state.players[player].accountBalance}M`}</p>
               </div>
             ))
             : 'Loading...'}
         </div>
-        <div className="center--dashboard__player">
-          Your information:
+        <div className="center--dashboard__block">
+          <h3 className="center--dashboard__title">Actions:</h3>
         </div>
         {state.loaded
           && state.boardState.currentPlayer.id === playerId
@@ -41,7 +41,7 @@ export default function Dashboard() {
           )
           : <></>}
         <button className="button__end-turn" type="button" onClick={() => socketFunctions.endTurn()}>
-          TURN
+          End turn
         </button>
       </section>
     </>
