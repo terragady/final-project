af995bd393b3eb4e8b10d9f23173de7c7fa76901
Marcin <terragady@gmail.com>
2020-07-28 15:12:38 +0200

    deploy :fire:

--- client/src/components/Tile.jsx
+++ client/src/components/Tile.jsx
@@ -11,24 +11,47 @@ function Tile({ initState, id, position }) {
 
   const handleCardClick = () => {
     setCardClicked({ clicked: !cardClicked.clicked });
+  };
 
-if (!cardClicked.clicked) {
-  return (
-    <article role="presentation" onClick={handleCardClick} className={`Tile tile${id} ${position}`} id={id}>
-      {initState.color && initState.color !== 'railroad'
-        ? (
-          <>
+  if (!cardClicked.clicked) {
+    return (
+      <article role="presentation" onClick={handleCardClick} className={`Tile tile${id} ${position}`} id={id}>
+        {initState.color && initState.color !== 'railroad'
+          ? (
+            <>
+              <div
+                className="tile__color-box"
+                style={
+                  state.loaded
+                    && Object.prototype.hasOwnProperty.call(state.boardState.ownedProps, id)
+                    ? { backgroundColor: initState.color, boxShadow: `0px 0px 1px 3px ${state.boardState.ownedProps[id].color}` }
+                    : { backgroundColor: initState.color }
+                }
+              />
+              <div className="tile__wrapper">
+                <p className="tile__street-name">{initState.streetName}</p>
+                <div className="player__token--wrapper">
+                  {Object.keys(state.players).map(e => (
+                    state.players[e].currentTile === id
+                      ? <div key={uuid()} className="player__token" style={{ backgroundColor: state.players[e].color }} />
+                      : <div key={uuid()} />
+                  ))}
+                </div>
+                <p className="tile__price">{`$${initState.price}M`}</p>
+              </div>
+            </>
+          )
+          : (
             <div
-              className="tile__color-box"
+              className="tile__special--wrapper"
               style={
                 state.loaded
                   && Object.prototype.hasOwnProperty.call(state.boardState.ownedProps, id)
-                  ? { backgroundColor: initState.color, boxShadow: `0px 0px 1px 3px ${state.boardState.ownedProps[id].color}` }
-                  : { backgroundColor: initState.color }
+                  ? { boxShadow: `0px 0px 1px 3px ${state.boardState.ownedProps[id].color}` }
+                  : {}
               }
-            />
-            <div className="tile__wrapper">
-              <p className="tile__street-name">{initState.streetName}</p>
+            >
+              <p className="tile__special-name">{initState.streetName}</p>
               <div className="player__token--wrapper">
                 {Object.keys(state.players).map(e => (
                   state.players[e].currentTile === id
@@ -36,36 +59,14 @@ if (!cardClicked.clicked) {
                     : <div key={uuid()} />
                 ))}
               </div>
-              <p className="tile__price">{`$${initState.price}M`}</p>
+              <p className="tile__special--price">{initState.price ? `$${initState.price}M` : ''}</p>
             </div>
-          </>
-        )
-        : (
-          <div
-            className="tile__special--wrapper"
-            style={
-              state.loaded
-                && Object.prototype.hasOwnProperty.call(state.boardState.ownedProps, id)
-                ? { boxShadow: `0px 0px 1px 3px ${state.boardState.ownedProps[id].color}` }
-                : {}
-            }
-          >
-            <p className="tile__special-name">{initState.streetName}</p>
-            <div className="player__token--wrapper">
-              {Object.keys(state.players).map(e => (
-                state.players[e].currentTile === id
-                  ? <div key={uuid()} className="player__token" style={{ backgroundColor: state.players[e].color }} />
-                  : <div key={uuid()} />
-              ))}
-            </div>
-            <p className="tile__special--price">{initState.price ? `$${initState.price}M` : ''}</p>
-          </div>
-        )}
-    </article>
-  );
-} else {
-  return <BackOfCard id={id} handleCardClick={handleCardClick} />;
-}
+          )}
+      </article>
+    );
+  } else {
+    return <BackOfCard id={id} handleCardClick={handleCardClick} />;
+  }
 }
 
 Tile.propTypes = {
