41985d1e0e275151f842a628e1dd14713af03d09
Marcin <terragady@gmail.com>
2020-07-29 18:56:39 +0200

    colored names in chat

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ io.on('connection', socket => {
 
   // send chat
   socket.on('send chat', message => {
-    sendToLog(`${state.players[socket.id].name} says: ${message}`)
+    sendToLog(`<span style="color:${state.players[socket.id].color}" class="log-name" >${state.players[socket.id].name}</span> says: ${message}`)
     io.emit('update', state);
   });
 
